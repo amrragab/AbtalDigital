1e857330e2bc1fb74018c0d6480266f9610da8f3
Ahmed Kamal <ahmed.kamal1432@gmail.com>
2014-11-10 00:42:39 +0200

    add part two picture i report

--- image/Proj1B/ldimage.m
+++ image/Proj1B/ldimage.m
@@ -13,34 +13,34 @@ figure(2);
 
 M = ones(3,3);
 M = ( 1 / 9 ) * M;
-subplot(3,1,1); imshow(M);
+subplot(1,3,1); imshow(M);
 # im after convlution
 R = imfilter(im, M);
-subplot(3,1,2); imshow(R,[]);
+subplot(1,3,2); imshow(R,[]);
 
 # M : inverse fourier of W
 W = fft2(M);
 W = fftshift(W);
 W = abs(W);
 W = log( W + 1);
-subplot(3,1,3); imshow(W, []);
+subplot(1,3,3); imshow(W, []);
 
 # second filter
 figure(3);
 
 M = ones(7,7);
 M = ( 1 / 49 ) * M;
-subplot(3,1,1); imshow(M);
+subplot(1,3,1); imshow(M);
 # im after convlution
 R = imfilter(im, M);
-subplot(3,1,2); imshow(R,[]);
+subplot(1,3,2); imshow(R,[]);
 
 # M : inverse fourier of W
 W = fft2(M);
 W = fftshift(W);
 W = abs(W);
 W = log( W + 1);
-subplot(3,1,3); imshow(W, []);
+subplot(1,3,3); imshow(W, []);
 
 # third filter
 
@@ -48,17 +48,17 @@ figure(4);
 
 M = ones(11,11);
 M = ( 1 / 11*11 ) * M;
-subplot(3,1,1); imshow(M);
+subplot(1,3,1); imshow(M);
 # im after convlution
 R = imfilter(im, M);
-subplot(3,1,2); imshow(R,[]);
+subplot(1,3,2); imshow(R,[]);
 
 # M : inverse fourier of W
 W = fft2(M);
 W = fftshift(W);
 W = abs(W);
 W = log( W + 1);
-subplot(3,1,3); imshow(W, []);
+subplot(1,3,3); imshow(W, []);
 
 # fourth filter
 
@@ -66,33 +66,33 @@ figure(5);
 
 M = ones(15,15);
 M = ( 1 / 15*15 ) * M; 
-subplot(3,1,1); imshow(M,[]);
+subplot(1,3,1); imshow(M,[]);
 # im after convlution
 R = imfilter(im, M);
-subplot(3,1,2); imshow(R, []);
+subplot(1,3,2); imshow(R, []);
 
 # M : inverse fourier of W
 W = fft2(M);
 W = fftshift(W);
 W = abs(W);
 W = log( W + 1);
-subplot(3,1,3); imshow(W, []);
+subplot(1,3,3); imshow(W, []);
 
 
 # fifth filter
 
-figure(5);
+figure(6);
 
 M = ones(21,21);
 M = ( 1 / 21*21 ) * M;
-subplot(3,1,1); imshow(M);
+subplot(1,3,1); imshow(M);
 # im after convlution
 R = imfilter(im, M);
-subplot(3,1,2); imshow(R,[]);
+subplot(1,3,2); imshow(R,[]);
 
 # M : fourier of W
 W = fft2(M);
 W = fftshift(W);
 W = abs(W);
 W = log( W + 1);
-subplot(3,1,3); imshow(W, []);
+subplot(1,3,3); imshow(W, []);
